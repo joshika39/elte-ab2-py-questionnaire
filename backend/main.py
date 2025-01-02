86a7876ba058cea086ade38ad4147478a8bf1812
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-01-03 00:41:25 +0100

    refactor: separated project into backend-frontend

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,19 @@
+import os
 import re
 from typing import List
 
 class Question:
-    def __init__(self, question: str, points: int, answer: str):
+    def __init__(self, number, question: str, points: int, answer: str):
         self.question = question.strip()
         self.points = points
         self.answer = answer.strip()
+        self.number = number
 
     def __repr__(self):
         return f"Question(points={self.points}, question={self.question[:30]}...)"
+
+    def __str__(self):
+        return f"{self.number}.\t{self.question}"
 
 def parse_question_pool(file_content: str) -> List[Question]:
     question_pattern = re.compile(r"## (\d+)\. (.*?)\((\d+) pont\)\n(.*?)(?=\n## |\Z)", re.DOTALL)
@@ -19,22 +24,31 @@ def parse_question_pool(file_content: str) -> List[Question]:
         question_text = match.group(2)
         points = int(match.group(3))
         answer = match.group(4)
-        questions.append(Question(question_text, points, answer))
+        questions.append(Question(question_number, question_text, points, answer))
     
     return questions
 
 def filter_questions_by_difficulty(questions: List[Question], points: int) -> List[Question]:
     return [q for q in questions if q.points == points]
 
+def print_questions(questions: List[Question]):
+    ordered_questions = sorted(questions, key=lambda q: q.number)
+    for q in ordered_questions:
+        print(q)
+
 if __name__ == "__main__":
-    with open("question_pool.txt", "r", encoding="utf-8") as file:
+    with open("./questions/kerdesek.md", "r", encoding="utf-8") as file:
         file_content = file.read()
 
     questions = parse_question_pool(file_content)
 
-    easy_questions = filter_questions_by_difficulty(questions, 1)
-    
-    print(f"Total Questions: {len(questions)}")
-    print(f"Easy Questions: {len(easy_questions)}")
-    for q in easy_questions[:5]:  # Display the first 5 easy questions
-        print(q)
+    while True:
+        num = input("Please enter the difficulty level: ")
+        try:
+            easy_questions = filter_questions_by_difficulty(questions, int(num))
+            os.system("clear")
+            print_questions(easy_questions)
+        except ValueError:
+            if num == "q":
+                exit()
+
